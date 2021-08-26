e3f54d57c172707b330c880d6f448a14af15e155
Song-Sujin <ssj1744@naver.com>
2021-08-26 15:55:23 +0900

    4.1 fin

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
-  
-  add = () => {
-    this.setState(current => ({count: current.count + 1}));
-  };
-  minus = () => {
-    this.setState({count: this.state.count - 1});
-  };
+  getMovies = async () => { // axios가 끝날때 까지 기다렸다가 계속해
+    const {data: { data : {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    //console.log(movies);
+    this.setState({movies, isLoading: false});  // 다 가지고 오면 false로 바꾸기
+  }
   componentDidMount() {
-    console.log("component rendered");
+    this.getMovies();
   }
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-  componentWillUnmount() {
-    console.log("Goodbye, cruel world");
-  }
-
   render() {
-    console.log("I'm redering");
+    const {isLoading, movies} = this.state;
     return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
+      <section class="container">
+        {isLoading ? 
+        (<div class="loader"><span class="loader__text">Loading...</span></div>) 
+        : 
+        movies.map(movie => // map()은 return이 반드시 필요. => 는 return이 포함되어 있음. map안에서의 movie는 내가 방금 만든 이름
+          <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}/>
+        )}
+      </section>
     )
   }
 }
